f6791cab646f117db43076c4d7e727b52ee41abb
Felix <65565033+fexfl@users.noreply.github.com>
2022-06-22 19:32:43 +0200

    Create PEP4.10.2 Elektron-Kern Streuung.m

---
+++ PEP4.10.2 Elektron-Kern Streuung.m
@@ -0,0 +1,16 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*f[q_]=3*(Sin[q*R/h]-q*R/h * Cos[x])/((q*R/h)^3)*)
+
+
+(* ::Input:: *)
+(*Minimize[f,q]*)
+
+
+(* ::Input:: *)
+(*g[x_]=3*(Sin[x]-x*Cos[x])/x^3*)
+
+
+(* ::Input:: *)
+(*Minimize[g,x]*)
