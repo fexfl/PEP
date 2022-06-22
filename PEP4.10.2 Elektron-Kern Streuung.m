fa41455219576436ce07afa955c67ed7484ef331
Felix <65565033+fexfl@users.noreply.github.com>
2022-06-22 20:07:08 +0200

    Update PEP4.10.2 Elektron-Kern Streuung.m

--- PEP4.10.2 Elektron-Kern Streuung.m
+++ PEP4.10.2 Elektron-Kern Streuung.m
@@ -1,26 +1,21 @@
 (* ::Package:: *)
 
 (* ::Input:: *)
-(*f[q_]=3*(Sin[q*R/h]-q*R/h * Cos[x])/((q*R/h)^3)*)
-
-
-(* ::Input:: *)
-(*Minimize[f,q]*)
-
-
-(* ::Input:: *)
 (*g[x_]=3*(Sin[x]-x*Cos[x])/x^3*)
+(*h[x_]=g[x]^2*)
 
 
 (* ::Input:: *)
-(*Minimize[g,x]*)
+(*rootx =Part[FindRoot[h[x],{x,4}],1]*)
 
 
 (* ::Input:: *)
-(*plt=Plot[g[x],{x,-10,10},PlotLabels->"Expressions"]*)
-(*plt2=ListPlot[{{-12/5,g[-12/5]}}]*)
-(*Show[plt,plt2]*)
+(*plt = Plot[h[x], {x, -10, 10}, PlotLabels -> "Expressions"]*)
+(**)
+(*plt2 = ListPlot[{{4.49341, h[4.49341]}}]*)
+(**)
+(*Show[plt, plt2]*)
 
 
 (* ::Input:: *)
-(*g[-12/5]*)
+(*h[4.49341]*)
